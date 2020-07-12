562f10f823f304232fe30982703be76554af35bd
vesperjahid <vesperjahid@gmail.com>
2020-07-13 01:05:32 +0600

    Menu & main page complete

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const menubtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menunav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+
+let showMenu = false;
+
+menubtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menunav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+    } else {
+        
+    hamburger.classList.remove('open');
+    menunav.classList.remove('open');
+    nav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+    
+    showMenu = false;
+    }
+}
